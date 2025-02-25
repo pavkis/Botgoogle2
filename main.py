2909d3ad0a3dc085b4e552eacf21f27a5c622054
pavkis <pkiselev87@gmail.com>
2025-02-25 15:01:20 +0000

    maestro pro

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ group_stopwords = {
 
 # Dynamic target group mapping
 group_config = {
-    (7855397066,-4543940911): [5486942816,1002439087307],  # Smart Money @oandmlis to Maestro and Feed for getTokenHolders 3 wallets
+    (7855397066,-4543940911): [6006866508,1002439087307],  # Smart Money @oandmlis to Maestro Pro and Feed for getTokenHolders 3 wallets
     (7855397066,-4559220661): [7753411011],  # Smart Money @oandmlis to Nova 4 wallets
     (7855397066,-4542478386): [7497120111],  # Smart Money @oandmlis to Bloom 5 wallets
     (-1002409298826): [4537474080,1002439087307],  #test source to target and feed
