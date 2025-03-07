25e9854786bf117c1633a7bcc3de9d0b1f776ddd
pavkis <pkiselev87@gmail.com>
2025-03-07 13:49:11 +0000

    7 wallets ozmo

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ logging.debug(f"Loaded API_ID: {API_ID}, Source IDs: {source_ids}")
 
 # Keywords for source groups
 group_filters = {
-    (7855397066,-4585305140): ["swapped"],  # Smart Money, SM Mirror 6
+    (7855397066,-4585305140): ["7 wallets"],  # Smart Money, SM Mirror 6
     (7855397066,-4550386211): ["2 wallets"],  # Smart Money, SM Mirror 2
     (7855397066,-4543940911): ["3 wallets"],  # Smart Money, SM Mirror 3
     (7855397066,-4559220661): ["4 wallets"],  # Smart Money, SM Mirror 4
@@ -40,16 +40,16 @@ group_filters = {
 
 # Stop-words for source groups
 group_stopwords = {
-    (7855397066): ["12 wallets","22 wallets","32 wallets","42 wallets","52 wallets","62 wallets","72 wallets","82 wallets","92 wallets","13 wallets","23 wallets","33 wallets","43 wallets","53 wallets","63 wallets","73 wallets","83 wallets","93 wallets","14 wallets","24 wallets","34 wallets","44 wallets","54 wallets","64 wallets","74 wallets","84 wallets","94 wallets","15 wallets","25 wallets","35 wallets","45 wallets","55 wallets","65 wallets","75 wallets","85 wallets","95 wallets","EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"],  #USDC
+    (7855397066): ["12 wallets","22 wallets","32 wallets","42 wallets","52 wallets","62 wallets","72 wallets","82 wallets","92 wallets","13 wallets","23 wallets","33 wallets","43 wallets","53 wallets","63 wallets","73 wallets","83 wallets","93 wallets","14 wallets","24 wallets","34 wallets","44 wallets","54 wallets","64 wallets","74 wallets","84 wallets","94 wallets","15 wallets","25 wallets","35 wallets","45 wallets","55 wallets","65 wallets","75 wallets","85 wallets","95 wallets","17 wallets","27 wallets","37 wallets","47 wallets","57 wallets","67 wallets","77 wallets","87 wallets","97 wallets","EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"],  #USDC
     (-1002409298826): ["13 wallets","23 wallets","33 wallets","43 wallets"],  #source test
 }
 
 # Dynamic target group mapping
 group_config = {
-    (7855397066,-4585305140): [1002280691008], # Smart Money @oandmlis to OZMO feed 4 wallets
+    (7855397066,-4585305140): [1002280691008], # Smart Money @oandmlis to OZMO feed 7 wallets
     (7855397066,-4550386211): [1002385053109], # Smart Money @oandmlis to OZMO feed 2 wallets
-    (7855397066,-4543940911): [6006866508,1002439087307],  # Smart Money @oandmlis to Maestro Pro and Feed for getTokenHolders 3 wallets
-    (7855397066,-4559220661): [7753411011,1002280691008],  # Smart Money @oandmlis to Nova 4 wallets and OZMO Feed 4 wallets
+    (7855397066,-4543940911): [6006866508,1002439087307],  # Smart Money @oandmlis to Maestro Pro 3 wallets and Feed for getTokenHolders 3 wallets
+    (7855397066,-4559220661): [7753411011],  # Smart Money @oandmlis to Nova 4 wallets
     (7855397066,-4542478386): [7497120111],  # Smart Money @oandmlis to Bloom 5 wallets
     (-1002409298826): [4537474080,1002439087307,1002385053109,1002280691008],  #test source to target and feed and ozmo
 }
